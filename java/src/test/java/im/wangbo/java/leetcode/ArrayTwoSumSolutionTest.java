50a40d3f9e756770ae6aaaf128b088860089bf0b
Elvis Wang <wangbo12@58ganji.com>
2019-05-01 18:23:49 +0800

    Add ArrayTwoSumSolution and its test

---
+++ java/src/test/java/im/wangbo/java/leetcode/ArrayTwoSumSolutionTest.java
@@ -0,0 +1,33 @@
+package im.wangbo.java.leetcode;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+/**
+ * TODO add brief description here
+ *
+ * @author Elvis Wang
+ */
+class ArrayTwoSumSolutionTest {
+    @ParameterizedTest
+    @MethodSource("arguments")
+    void test(final int[] arr, final int target, final int[] expected) {
+        final ArrayTwoSumSolution s = new ArrayTwoSumSolution();
+
+        final int[] result = s.twoSum(arr, target);
+
+        Assertions.assertThat(result).containsExactly(expected);
+    }
+
+    static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 2, 1}, 4, new int[]{1, 2}),
+                Arguments.of(new int[]{3, 2, 4}, 6, new int[]{1, 2}),
+                Arguments.of(new int[]{2, 7, 11, 15}, 9, new int[]{0, 1})
+        );
+    }
+}
