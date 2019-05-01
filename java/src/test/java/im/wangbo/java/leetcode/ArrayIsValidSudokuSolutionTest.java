8c9b400f2eb751959d8c9620e43ff5ba46cd01c2
Elvis Wang <wangbo12@58ganji.com>
2019-05-01 19:03:54 +0800

    Add ArrayIsValidSudokuSolution and its test

---
+++ java/src/test/java/im/wangbo/java/leetcode/ArrayIsValidSudokuSolutionTest.java
@@ -0,0 +1,61 @@
+package im.wangbo.java.leetcode;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+/**
+ * TODO add brief description here
+ *
+ * @author Elvis Wang
+ */
+class ArrayIsValidSudokuSolutionTest {
+    @Test
+    void test() {
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("arguments")
+    void test(final char[][] arr, final boolean expected) {
+        final ArrayIsValidSudokuSolution s = new ArrayIsValidSudokuSolution();
+
+        final boolean result = s.isValidSudoku(arr);
+
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> arguments() {
+
+        char[][] data1 = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        char[][] data2 = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '9', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        return Stream.of(
+                Arguments.of(data1, true),
+                Arguments.of(data2, false)
+        );
+    }
+}
