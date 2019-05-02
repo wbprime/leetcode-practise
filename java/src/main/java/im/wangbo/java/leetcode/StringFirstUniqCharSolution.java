2446132179eb1d800c7df2fec690d46a52c20143
Elvis Wang <wangbo12@58ganji.com>
2019-05-02 13:02:06 +0800

    Add StringFirstUniqCharSolution and its test

---
+++ java/src/main/java/im/wangbo/java/leetcode/StringFirstUniqCharSolution.java
@@ -0,0 +1,47 @@
+package im.wangbo.java.leetcode;
+
+/**
+ * See https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/34/
+ *
+ * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+ *
+ * 案例:
+ *
+ * s = "leetcode"
+ * 返回 0.
+ *
+ * s = "loveleetcode",
+ * 返回 2.
+ *
+ *
+ * 注意事项：您可以假定该字符串只包含小写字母。
+ *
+ * @author Elvis Wang
+ */
+public class StringFirstUniqCharSolution {
+    public int firstUniqChar(final String s) {
+        final int len = s.length();
+        if (len == 0) return -1;
+
+        final int[] indices = new int[26];
+        for (int i = 0; i < 26; i++) {
+            indices[i] = 0;
+        }
+
+        for (int i = 0; i < len; i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                indices[ch - 'a']++;
+            }
+        }
+
+        for (int i = 0; i < len; i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                if (indices[ch - 'a'] == 1) return i;
+            }
+        }
+        return -1;
+    }
+}
+
