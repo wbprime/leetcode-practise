a57f002c87f9593cbc34b1f7c2d3866996766d60
Elvis Wang <wangbo12@58ganji.com>
2019-05-02 16:08:37 +0800

    Add LinkedListMergeTwoListsSolution and its test

---
+++ java/src/main/java/im/wangbo/java/leetcode/LinkedListMergeTwoListsSolution.java
@@ -0,0 +1,55 @@
+package im.wangbo.java.leetcode;
+
+/**
+ * See https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/6/linked-list/44/
+ *
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ * @author Elvis Wang
+ */
+class LinkedListMergeTwoListsSolution {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(final int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (null == l1) return l2;
+        if (null == l2) return l1;
+
+        ListNode lcur = l1;
+        ListNode rcur = l2;
+
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+
+        while (lcur != null && rcur != null) {
+            if (lcur.val < rcur.val) {
+                cur.next = lcur;
+                lcur = lcur.next;
+            } else {
+                cur.next = rcur;
+                rcur = rcur.next;
+            }
+            cur = cur.next;
+        }
+
+        if (lcur == null) {
+            cur.next = rcur;
+        }
+        if (rcur == null) {
+            cur.next = lcur;
+        }
+
+        return head.next;
+    }
+}
---
+++ java/src/test/java/im/wangbo/java/leetcode/LinkedListMergeTwoListsSolutionTest.java
@@ -0,0 +1,64 @@
+package im.wangbo.java.leetcode;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * TODO add brief description here
+ *
+ * @author Elvis Wang
+ */
+class LinkedListMergeTwoListsSolutionTest {
+    @Test
+    void test() {
+        final LinkedListMergeTwoListsSolution.ListNode head1;
+        final int n1;
+        final LinkedListMergeTwoListsSolution.ListNode head2;
+        final int n2;
+        {
+            n1 = ThreadLocalRandom.current().nextInt(100) + 1;
+            LinkedListMergeTwoListsSolution.ListNode node = new LinkedListMergeTwoListsSolution.ListNode(-1);
+            head1 = node;
+
+            for (int i = 0; i < n1; i++) {
+                LinkedListMergeTwoListsSolution.ListNode next = new LinkedListMergeTwoListsSolution.ListNode(i * 2 + 1);
+                node.next = next;
+                node = next;
+            }
+            node.next = null;
+        }
+        {
+            n2 = ThreadLocalRandom.current().nextInt(100) + 1;
+            LinkedListMergeTwoListsSolution.ListNode node = new LinkedListMergeTwoListsSolution.ListNode(-2);
+            head2 = node;
+
+            for (int i = 0; i < n2; i++) {
+                LinkedListMergeTwoListsSolution.ListNode next = new LinkedListMergeTwoListsSolution.ListNode(i * 2);
+                node.next = next;
+                node = next;
+            }
+            node.next = null;
+        }
+
+        final LinkedListMergeTwoListsSolution s = new LinkedListMergeTwoListsSolution();
+
+        final LinkedListMergeTwoListsSolution.ListNode result = s.mergeTwoLists(head1, head2);
+
+        {
+            LinkedListMergeTwoListsSolution.ListNode tmp = result;
+            int min = Integer.MIN_VALUE;
+            int count = 0;
+            while (null != tmp) {
+                Assertions.assertThat(tmp.val).isGreaterThan(min);
+                min = tmp.val;
+
+                tmp = tmp.next;
+
+                count++;
+            }
+            Assertions.assertThat(count).isEqualTo(n1 + n2 + 2); // plus 2 dummy head
+        }
+    }
+}
