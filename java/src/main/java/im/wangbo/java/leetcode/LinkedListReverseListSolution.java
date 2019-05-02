f1b79b636740775a9aa4ab36e3cb248d60f572b2
Elvis Wang <wangbo12@58ganji.com>
2019-05-02 15:42:13 +0800

    Add LinkedListReverseListSolution and its test

---
+++ java/src/main/java/im/wangbo/java/leetcode/LinkedListReverseListSolution.java
@@ -0,0 +1,43 @@
+package im.wangbo.java.leetcode;
+
+/**
+ * See https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/6/linked-list/43/
+ *
+ * 反转一个单链表。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ *
+ * @author Elvis Wang
+ */
+public class LinkedListReverseListSolution {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(final int v) {
+            this.val = v;
+        }
+    }
+
+    public ListNode reverseList(final ListNode head) {
+        if (null == head || head.next == null) return head;
+
+        ListNode cur = head;
+        ListNode next = cur.next;
+        cur.next = null;
+        while (null != next) {
+            ListNode nnext = next.next;
+
+            next.next = cur;
+            cur = next;
+            next = nnext;
+        }
+
+        return cur;
+    }
+}
---
+++ java/src/test/java/im/wangbo/java/leetcode/LinkedListReverseListSolutionTest.java
@@ -0,0 +1,40 @@
+package im.wangbo.java.leetcode;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * TODO add brief description here
+ *
+ * @author Elvis Wang
+ */
+class LinkedListReverseListSolutionTest {
+    @Test
+    void test() {
+        final int n = ThreadLocalRandom.current().nextInt(100) + 1;
+        LinkedListReverseListSolution.ListNode node = new LinkedListReverseListSolution.ListNode(-1);
+        LinkedListReverseListSolution.ListNode head = node;
+
+        for (int i = 0; i < n; i++) {
+            LinkedListReverseListSolution.ListNode next = new LinkedListReverseListSolution.ListNode(i);
+            node.next = next;
+            node = next;
+        }
+        node.next = null;
+
+        final LinkedListReverseListSolution s = new LinkedListReverseListSolution();
+
+        final LinkedListReverseListSolution.ListNode result = s.reverseList(head);
+
+        {
+            LinkedListReverseListSolution.ListNode tmp = result;
+            for (int i = n - 1; i >= -1; i--) {
+                Assertions.assertThat(tmp.val).isEqualTo(i);
+                tmp = tmp.next;
+            }
+            Assertions.assertThat(tmp).isNull();
+        }
+    }
+}
