ae7031effa04c7e34291720016025eeb5b62ae56
Elvis Wang <wangbo12@58ganji.com>
2019-05-03 17:36:25 +0800

    Add ValidBracketSolution and its test

---
+++ java/src/main/java/im/wangbo/java/leetcode/design/ValidBracketSolution.java
@@ -0,0 +1,74 @@
+package im.wangbo.java.leetcode.design;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * See https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/26/others/68/
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * 示例 1:
+ *
+ * 输入: "()"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: "()[]{}"
+ * 输出: true
+ * 示例 3:
+ *
+ * 输入: "(]"
+ * 输出: false
+ * 示例 4:
+ *
+ * 输入: "([)]"
+ * 输出: false
+ *
+ * 示例 5:
+ *
+ * 输入: "{[]}"
+ * 输出: true
+ *
+ * @author Elvis Wang
+ */
+class ValidBracketSolution {
+    public boolean isValid(String s) {
+        int len = s.length();
+        if (len <= 0) return true;
+
+        final List<Character> stack = new ArrayList<>();
+        for (int i = 0; i < len; i++) {
+            final char ch = s.charAt(i);
+
+            switch (ch) {
+                case '(': // fall through
+                case '{': // fall through
+                case '[':
+                    stack.add(ch);
+                    break;
+                case ')': {
+                    if (stack.isEmpty() || stack.remove(stack.size() - 1) != '(') return false;
+                }
+                break;
+                case '}': {
+                    if (stack.isEmpty() || stack.remove(stack.size() - 1) != '{') return false;
+                }
+                break;
+                case ']': {
+                    if (stack.isEmpty() || stack.remove(stack.size() - 1) != '[') return false;
+                }
+                break;
+                default: // Do nothing
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
---
+++ java/src/test/java/im/wangbo/java/leetcode/design/ValidBracketSolutionTest.java
@@ -0,0 +1,36 @@
+package im.wangbo.java.leetcode.design;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+/**
+ * TODO add brief description here
+ *
+ * @author Elvis Wang
+ */
+class ValidBracketSolutionTest {
+    @ParameterizedTest
+    @MethodSource("arguments")
+    void test(final String a, final boolean expected) {
+        final ValidBracketSolution s = new ValidBracketSolution();
+
+        final boolean result = s.isValid(a);
+
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.of("{[]}", true),
+                Arguments.of("([)]", false),
+                Arguments.of("(]", false),
+                Arguments.of("()[]{}", true),
+                Arguments.of("", true),
+                Arguments.of("()", true)
+        );
+    }
+}
