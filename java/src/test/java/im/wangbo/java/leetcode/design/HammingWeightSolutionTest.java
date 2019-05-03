54f39a6b4d64a4794ad9172e5489bfb46c1178e4
Elvis Wang <wangbo12@58ganji.com>
2019-05-03 17:15:06 +0800

    Add HammingWeightSolution and its test

---
+++ java/src/test/java/im/wangbo/java/leetcode/design/HammingWeightSolutionTest.java
@@ -0,0 +1,33 @@
+package im.wangbo.java.leetcode.design;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+/**
+ * TODO add brief description here
+ *
+ * @author Elvis Wang
+ */
+class HammingWeightSolutionTest {
+    @ParameterizedTest
+    @MethodSource("arguments")
+    void test(final int a, final int expected) {
+        final HammingWeightSolution s = new HammingWeightSolution();
+
+        final int result = s.hammingWight(a);
+
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.of(-3, 31), // 0b11111111111111111111111111111101
+                Arguments.of(128, 1), // 0b00000000000000000000000010000000
+                Arguments.of(11, 3) // 0b00000000000000000000000000001011
+        );
+    }
+}
